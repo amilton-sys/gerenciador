2de4a0e41e98b9cf9a3f8c4f451aa1699747b6ff
Amilton <amiltonjose.pe@gmail.com>
2025-01-24 11:59:02 -0300

    init project

---
+++ src/main/java/com/sys/gerenciador/GerenciadorApplication.java
@@ -0,0 +1,11 @@
+package com.sys.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorApplication.class, args);
+	}
+}
